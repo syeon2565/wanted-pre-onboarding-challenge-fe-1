268dd56c33d2e0babb377c045160532d2718970d
seongyeon <ysy2565@naver.com>
2022-07-28 00:14:25 +0900

    feat: 로그아웃구현

--- src/App.js
+++ src/App.js
@@ -3,11 +3,24 @@ import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
 
 function App() {
+  const handleLogOut = () => {
+    localStorage.removeItem("token");
+    alert("로그아웃 되었습니다.");
+  };
+  const token = localStorage.getItem("token");
   return (
-    <Routes>
-      <Route path="/auth/login" element={<Login />} />
-      <Route path="/auth/signUp" element={<SignUp />} />
-    </Routes>
+    <>
+      {token && (
+        <button type="button" onClick={handleLogOut}>
+          로그아웃
+        </button>
+      )}
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/auth/login" element={<Login />} />
+        <Route path="/auth/signUp" element={<SignUp />} />
+      </Routes>
+    </>
   );
 }
 
